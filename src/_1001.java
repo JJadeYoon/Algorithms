bf3f3c36764d5711cdf7cbe9eea46b02e5bc1083
JJadeYoon <yess9904@naver.com>
2023-07-16 22:52:32 +0900

    2023.7.16 baekjoon1001

--- src/_1001.java
+++ src/_1001.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class _1001 {
+import java.util.Scanner;
+
+public class _1001 {
+    public static void main(String[] args) {
+        int A, B;
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            A = sc.nextInt();
+        } while (A <= 0);
+
+        do {
+            B = sc.nextInt();
+        } while (B >= 10);
+
+        System.out.println(A - B);
+    }
 }
